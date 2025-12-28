e181b631f3cbc08b10bbc1427eed6095d734733a
wellitsabhi <abhishekongit@gmail.com>
2025-12-28 14:48:48 +0530

    14: api tool

---
+++ src/app/api/api-tools/route.ts
@@ -0,0 +1,62 @@
+import {
+  UIMessage,
+  InferUITools,
+  UIDataTypes,
+  streamText,
+  convertToModelMessages,
+  tool,
+  stepCountIs,
+} from "ai";
+import { groq } from "@ai-sdk/groq";
+import { z } from "zod";
+
+const tools = {
+  getWeather: tool({
+    description: "Get the weather for a location",
+    inputSchema: z.object({
+      city: z.string().describe("The city to get the weather for"),
+    }),
+    execute: async ({ city }) => {
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${city}`
+      );
+      const data = await response.json();
+      const weatherData = {
+        location: {
+          name: data.location.name,
+          country: data.location.country,
+          localtime: data.location.localtime,
+        },
+        current: {
+          temp_c: data.current.temp_c,
+          condition: {
+            text: data.current.condition.text,
+            code: data.current.condition.code,
+          },
+        },
+      };
+      return weatherData;
+    },
+  }),
+};
+
+export type ChatTools = InferUITools<typeof tools>;
+export type ChatMessage = UIMessage<never, UIDataTypes, ChatTools>;
+
+export async function POST(req: Request) {
+  try {
+    const { messages }: { messages: ChatMessage[] } = await req.json();
+
+    const result = streamText({
+      model: groq("openai/gpt-oss-20b"),
+      messages: await convertToModelMessages(messages),
+      tools,
+      stopWhen: stepCountIs(2),
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (error) {
+    console.error("Error in /api/chat:", error);
+    return new Response("Failed to stream chat messages", { status: 500 });
+  }
+}
